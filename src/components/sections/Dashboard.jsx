d133cbcfeda4a0c08a9956c6dee6754d0541eddd
ChadCasey <gbchadd@gmail.com>
2025-02-01 16:30:59 -0700

    minor optimizations

--- src/components/sections/Dashboard.jsx
+++ src/components/sections/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <section className="relative">
       <div className="bottom-0 -z-10 absolute bg-primary-500 w-full h-1/2"></div>
       <div className="justify-items-center grid m-auto px-4 sm:px-8 md:px-16 xl:px-24 py-8 md:py-16 max-w-[90rem]">
-        <img src={dashboardImage} className="block w-full" />
+        <img src={dashboardImage} alt="Dashboard" className="block w-full" />
       </div>
     </section>
   );
